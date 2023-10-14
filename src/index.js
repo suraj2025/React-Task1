5cc307cbee18574f8c38df2613164cd351abdf01
Suraj <sk5195609@gmail.com>
2023-10-14 14:36:45 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -8,10 +8,5 @@ ReactDOM.render(
  <App />,document.getElementById('root')
 );
 
-// echo "# React-Task1" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/suraj2025/React-Task1.git
-// git push -u origin main
+
+
